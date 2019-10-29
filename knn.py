55a5d58b162659d99272c6cb75dd002a5cccd978
LuPei9ing <30286145+LuPei9ing@users.noreply.github.com>
2019-10-28 22:11:32 -0400

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,13 +1,44 @@
+import math
+
 class KNeighborsClassifier:
 
     def __init__(self, k):
         self.k = k
+    def euclideanDistance(instance1, instance2, length):
+        distance = 0
+        for x in range(length):
+            distance += pow((instance1[x] - instance2[x]), 2)
+        return math.sqrt(distance)
 
-    def fit(self, X_train, y_train):
-        raise NotImplementedError()
+    def fit(self, trainingSet, testInstance, k):
+        distances = []
+        length = len(testInstance)-1
+        for x in range(len(trainingSet)):
+            dist = euclideanDistance(testInstance, trainingSet[x], length)
+            distances.append((trainingSet[x], dist))
+        distances.sort(key=operator.itemgetter(1))
+        neighbors = []
+        for x in range(k):
+            neighbors.append(distances[x][0])
+        return neighbors
 
-    def predict(self, X_test):
-        raise NotImplementedError()
+    //predict
+    def getResponse(neighbors):
+        classVotes = {}
+        for x in range(len(neighbors)):
+            response = neighbors[x][-1]
+            if response in classVotes:
+                classVotes[response] += 1
+            else:
+                classVotes[response] = 1
+        sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+        return sortedVotes[0][0]
+        
 
     def score(self, X_test, y_test):
-        raise NotImplementedError()
+        correct = 0
+        for x in range(len(X_test)):
+            if testSet[x][-1] is y_test[x]:
+                correct += 1
+        return (correct/float(len(X_test))) * 100.0
+    
